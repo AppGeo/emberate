f8d952cc9981ab4d7d4120036c9ffaf8d67aba2b
Ilya Radchenko <ilya@burstcreations.com>
2014-04-17 09:49:16 -0400

    minimum structure test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-var esg = require('../lib'),
-  fs = require('fs'),
-  path = require('path'),
-  test = require('tape'),
-  allStructure = path.resolve(__dirname, 'structures', 'all'),
-  generatedFile = path.resolve(__dirname, 'structures', 'all', '.index.js'),
-  expectedFile = path.resolve(__dirname, 'structures', 'all-structures.js');
-  console.log(__dirname);
+var esg = require('../lib');
+var fs = require('fs');
+var path = require('path');
+var test = require('tape');
+var allStructure = path.join(__dirname, 'structures', 'all');
+var minStructure = path.join(__dirname, 'structures', 'min');
+var generatedFile = path.join(__dirname, 'structures', 'all', '.index.js');
+var expectedFile = path.join(__dirname, 'structures', 'all-structures.js');
 
 test('is a stream', function (t) {
   var instance = esg(allStructure);
@@ -18,11 +18,26 @@ test('creates expected output', function (t) {
   var instance = esg(allStructure).pipe(fs.createWriteStream(generatedFile));
 
   instance.on('finish', function () {
-    debugger;
     fs.readFile(generatedFile, function (err, data) {
-      debugger;
       var expected = fs.readFileSync(expectedFile);
 
+      t.notOk(err, 'No file reading errors');
+      t.deepEqual(data, expected, 'Generated is same as expected');
+    });
+  });
+
+  t.end();
+});
+
+test('works with required dirs/files only', function (t) {
+  var expectedMinFile = path.join(__dirname, 'structures', 'min-structures.js');
+  var generatedMinFile = path.join(__dirname, 'structures', 'min', '.index.js');
+  var instance = esg(minStructure).pipe(fs.createWriteStream(generatedMinFile));
+
+  instance.on('finish', function () {
+    fs.readFile(generatedMinFile, function (err, data) {
+      var expected = fs.readFileSync(expectedMinFile);
+      
       t.notOk(err, 'No file reading errors');
       t.deepEqual(data, expected, 'Generated is same as expected');
     });
---
+++ test/structures/min-structures.js
@@ -0,0 +1,15 @@
+// this file is auto-generated, do not edit
+
+require('ember'); // get Ember global around for the templates
+require('./.templates');
+
+var routes = require('./config/routes');
+var App = require('./config/application');
+
+App.Router.map(routes);
+
+
+
+
+
+module.exports = App;
