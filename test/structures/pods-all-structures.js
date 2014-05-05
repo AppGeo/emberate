0ec46d0ec6bfa3a5a9812e26e77d1a3736195486
Calvin Metcalf <cmetcalf@appgeo.com>
2014-05-05 13:56:43 -0400

    fix ordering

--- test/structures/pods-all-structures.js
+++ test/structures/pods-all-structures.js
@@ -8,9 +8,9 @@ App.Router.map(require('./app/config/routes'));
 require('./app/helpers/ajax');
 
 
-Ember.TEMPLATES['user/index'] = require('./pods/user/index/template.hbs');
-Ember.TEMPLATES['sidebar'] = require('./app/templates/sidebar.hbs');
 Ember.TEMPLATES['application'] = require('./pods/application/template.hbs');
+Ember.TEMPLATES['sidebar'] = require('./app/templates/sidebar.hbs');
+Ember.TEMPLATES['user/index'] = require('./pods/user/index/template.hbs');
 App.TestInitializer = require('./app/initializers/test');
 App.TestMixin = require('./app/mixins/test');
 App.ObjectTransform = require('./app/transforms/object');
@@ -18,8 +18,8 @@ App.UserSerializer = require('./app/serializers/user');
 App.UserAdapter = require('./app/adapters/user');
 App.UserModel = require('./app/models/user');
 App.XPlayerComponent = require('./components/x-player/component');
-App.UserIndexController = require('./pods/user/index/controller');
 App.ApplicationController = require('./pods/application/controller');
+App.UserIndexController = require('./pods/user/index/controller');
 App.ApplicationRoute = require('./pods/application/route');
 App.ApplicationView = require('./pods/application/view');
 
