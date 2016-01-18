3b540f974ab1de8e87a17e0edca8eecff2843bd7
Travis Peterson <travis.peterson@ave81.com>
2016-01-18 12:13:58 -0600

    adds support for component pods

--- test/structures/pods-all/.index.js
+++ test/structures/pods-all/.index.js
@@ -13,6 +13,7 @@ App.Router = require('./router');
 
 
 Ember.TEMPLATES['application'] = require('./pods/application/template.hbs');
+Ember.TEMPLATES['components/component-pod'] = require('./components/component-pod/template.hbs');
 Ember.TEMPLATES['sidebar'] = require('./templates/sidebar.hbs');
 Ember.TEMPLATES['user/index'] = require('./pods/user/index/template.hbs');
 App.TestInitializer = require('./initializers/test');
@@ -21,6 +22,7 @@ App.ObjectTransform = require('./transforms/object');
 App.UserSerializer = require('./serializers/user');
 App.UserAdapter = require('./adapters/user');
 App.UserModel = require('./models/user');
+App.ComponentPodComponent = require('./components/component-pod/component');
 App.XPlayerComponent = require('./components/x-player');
 App.ApplicationController = require('./pods/application/controller');
 App.UserIndexController = require('./pods/user/index/controller');
--- test/structures/pods-custom/.index.js
+++ test/structures/pods-custom/.index.js
@@ -13,6 +13,7 @@ App.Router = require('./router');
 
 
 Ember.TEMPLATES['application'] = require('./foo/application/template.hbs');
+Ember.TEMPLATES['components/component-pod'] = require('./components/component-pod/template.hbs');
 Ember.TEMPLATES['sidebar'] = require('./templates/sidebar.hbs');
 Ember.TEMPLATES['user/index'] = require('./foo/user/index/template.hbs');
 App.TestInitializer = require('./initializers/test');
@@ -21,6 +22,7 @@ App.ObjectTransform = require('./transforms/object');
 App.UserSerializer = require('./serializers/user');
 App.UserAdapter = require('./adapters/user');
 App.UserModel = require('./models/user');
+App.ComponentPodComponent = require('./components/component-pod/component');
 App.XPlayerComponent = require('./components/x-player');
 App.ApplicationController = require('./foo/application/controller');
 App.UserIndexController = require('./foo/user/index/controller');
